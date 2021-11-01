ad6317720e20fa204f900b52c5621ac2eca1c66e
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-11-01 14:59:01 +0000

    Code Challenge

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(int1,int2){
+  if(int1 > int2){
+      console.log(int1 + " is the greater number and " + int2 + " is the smaller number");
+  } else
+  console.log (int2 + " is the greater number and " + int1 + " is the smaller number");
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(int1,int2,int3){
+  if(int1 > int2 && int1 > int3){
+      console.log(int1 + " is the greatest of all 3 numbers ");
+  } else if (int2 > int1 && int2> int3){
+      console.log(int2 + " is the greatest of all 3 numbers");
+  } else
+  console.log( int3 + " is the greates of all 3 numbers");
 }
 
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(int1,int2,int3,int4){
+   if(int1 > int2 && int1 > int3 && int1 > int4){
+      console.log(int1 + " is the greatest of all 3 numbers ");
+  } else if (int2 > int1 && int2> int3 && int2> int4){
+      console.log(int2 + " is the greatest of all 3 numbers");
+  } else if(int3 > int1 && int3 > int2 && int3 > int4){
+  console.log( int3 + " is the greates of all 3 numbers");
+} else {
+    console.log(int4 + " is the greatest of all 3 numbers");
 }
+}
+
 
 
 //4
 function leastOfFour(){
   
 }
+
+greatestOfTwo(60,50)
+greatestOfThree(2,4,30)
+greatestOfFour(3,4,50,6)
